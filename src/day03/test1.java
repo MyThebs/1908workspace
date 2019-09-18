f9482d14151505ff0cd2791cb7e8dedc6c63f982
thebs <841567334@qq.com>
2019-09-18 10:36:48 +0800

    hello

---
+++ src/day03/test1.java
@@ -0,0 +1,35 @@
+package day03;
+
+import java.util.Scanner;
+
+public class test1
+{
+    public void work1()
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println((a > b) ? a : b);
+    }
+
+    public void work2()
+    {
+        /*三目运算符的规则
+         * 1.值1和值2都是常量的情况下向上转型
+         * 2.值1和值2都是变量的情况下向上转型
+         * 3，值1值2一个常量一个变量，按照取值范围大的那个转型
+         * */
+        char a = 'a';
+        int b = 100;
+        System.out.println(false ? b : (char) (a + 5));
+        System.out.println(false ? 100 : (char) (a + 5));
+        System.out.println(false ? 65536 : (char) (a + 5));
+        System.out.println(1 + 2 + 'a' + '1' + 1 + 2);
+    }
+
+    public static void main(String[] args)
+    {
+        test1 test1 = new test1();
+        test1.work2();
+    }
+}
